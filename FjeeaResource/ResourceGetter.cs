84b2f3dc4b702feaa9a92e27cfcb1f2de8565474
zsx <zsx@zsxsoft.com>
2016-06-22 13:58:51 +0800

    Add FjeeaResource

---
+++ FjeeaResource/ResourceGetter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FjeeaResource
+{
+    public class ResourceGetter
+    {
+        public static ResourceSet GetResources()
+        {
+            return Properties.Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+        }
+    }
+}
