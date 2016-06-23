fd3a6081aef50b71848407f61355c86a915a3a78
zsx <zsx@zsxsoft.com>
2016-06-23 21:16:50 +0800

    The last update

--- FjeeaResource/Properties/Resources.Designer.cs
+++ FjeeaResource/Properties/Resources.Designer.cs
@@ -3183,6 +3183,16 @@ namespace FjeeaResource.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        public static System.Drawing.Bitmap _7_29 {
+            get {
+                object obj = ResourceManager.GetObject("_7_29", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         public static System.Drawing.Bitmap _7_3 {
             get {
                 object obj = ResourceManager.GetObject("_7_3", resourceCulture);
